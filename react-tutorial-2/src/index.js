51933e9b92bfa3d625a42fbd6e7da189c438ddf9
avicndugu <avicnduguh@gmail.com>
2020-03-30 21:46:52 +0300

    Has two pages views: innitial page and final page

--- react-tutorial-2/src/index.js
+++ react-tutorial-2/src/index.js
@@ -20,11 +20,46 @@ class App extends Component{
               "url":"img/shoes.png",
               "audio":"shoes.mp3"
             },
+            {
+              "name":"socks",
+              "url":"img/socks.png",
+              "audio":"socks.mp3"
+            },
+            {
+              "name":"star",
+              "url":"img/star.png",
+              "audio":"star.mp3"
+            },
+            {
+              "name":"sun",
+              "url":"img/sun.png",
+              "audio":"sun.mp3"
+            },
+            {
+              "name":"bottle",
+              "url":"img/bottle.png",
+              "audio":"bottle.mp3"
+            },
+            {
+              "name":"door",
+              "url":"img/door.png",
+              "audio":"door.mp3"
+            },
+            {
+              "name":"car",
+              "url":"img/car.png",
+              "audio":"car.mp3"
+            },
+            {
+              "name":"cup",
+              "url":"img/cup.png",
+              "audio":"cup.mp3"
+            }
         ]
-      return(
+        return(
             <div className="app">
                 <Header />
-                <Items allItems= {allItems}/>
+                <Items allItems= {allItems} />
             </div>
         )
     }
@@ -35,23 +70,33 @@ class Header extends Component{
         super(props);
         this.state={
             value:"Start",
-            count:0
+            count:0,
+            display:true,
         }
     }
     render(){
-        return(
-            <div className="header">
-                <h1 id="my-logo" className="title">Picture learner</h1>
-                <img src="img/explain-img.png" alt="Exciting learning"/>
-                <h2 className="subtitle">Learn through pictures and sounds</h2>
-                <button className="button" onClick={()=> this.setState({value:"Resume"})}>{this.state.value}</button>
-                <button className="button" onClick={()=> this.setState({count:1})}>{this.state.count}</button>
-            </div>
-        )
-    }
+        if(this.state.display===true){
+            return(
+                <div className="header">
+                    <h1 id="my-logo" className="title">Picture learner</h1>
+                    <img src="img/explain-img.png" alt="Exciting learning"/>
+                    <h2 className="subtitle">Learn through pictures and sounds</h2>
+                    <button className="button" onClick={()=> this.setState({value:"Resume"})}>{this.state.value}</button>
+                    <button className="button" onClick={()=> this.setState({count:1})}>{this.state.count}</button>
+                    <button className="button" onClick={()=> this.setState({display:false})}>{this.state.value}</button>
+                </div>
+            )
+        } else {
+            return(
+                <div className="header">
+                    <p>Common House Items</p>
+                </div>
+            )
+        }
+    }   
 }
 
-class Items extends Component{
+class Items extends Component {
     constructor(props){
         super(props);
             this.state={
@@ -59,21 +104,24 @@ class Items extends Component{
         };
     }
     increment(){
-        this.setState({
-            count: this.state.count + 1
-        })
+        if(this.state.count<9){
+            this.setState({
+                count: this.state.count + 1
+            })
+        }
     }
     decrement(){
-        this.setState({
-            count: this.state.count - 1
-        })
+        if(this.state.count>0){
+            this.setState({
+                count: this.state.count - 1
+            })
+        }
     }
 
     render(){
     const { allItems } = this.props
 
-    
-    
+
     const rows = this.props.allItems.map((row, index)=> {
         return (
             <tr key={index}>
@@ -89,11 +137,9 @@ class Items extends Component{
                 <p>Count {allItems[this.state.count].name}</p>
                 <p>Count {allItems[this.state.count].url}</p>
                 <p>Count {allItems[this.state.count].audio}</p>
-                <p className="title">Car</p>
-                <img src="img/explain-img.png" alt="items" />
                 <br/>
-                <button className="button" onClick={(e)=> this.increment(e)}>Next</button>
                 <button className="button" onClick={(e)=> this.decrement(e)}>Previous</button>
+                <button className="button" onClick={(e)=> this.increment(e)}>Next</button>
                 <table>
                     <thead>
                         <tr>
